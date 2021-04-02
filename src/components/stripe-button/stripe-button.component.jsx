0b862c30e71b4987fcbe8266c3bd037979a339bc
Kwok Ming Leung <k4leung@ucsd.edu>
2021-04-02 11:59:08 -0700

    updating stripe integration and favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51IbrJ2C7sdqQpIj9VDbvDYuyUERVHN8sW221Lkm0wEQq1xEzHVEXCKzLDCiHcBpBqqmC8wVXmeSxzGLzPlgWzPLI00ExAiurig";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
